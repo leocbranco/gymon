97c8a2a2f71e1d5426a7097b0baa76ebbee7410f
leocbranco <158091153+leocbranco@users.noreply.github.com>
2024-04-24 17:15:31 -0300

    update

--- login-personal.php
+++ login-personal.php
@@ -134,7 +134,7 @@
             <br>
             <button type="submit" name="login" value="entrar" class="register-button">Entrar</button>
         </form>
-        <p>Ainda não possui uma conta? <a href="registro-personal.php" class="login-link">Sign in</a></p>
+        <p>Ainda não possui uma conta? <a href="registro-personal.php" class="login-link">Sign up</a></p>
     </div>
 </div>
 </body>

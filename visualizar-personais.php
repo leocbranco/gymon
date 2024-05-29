abeb2acdfc129aa5e13ad7a5db8c5ce46ff9ef02
leocbranco <leonardobranco.work@gmail.com>
2024-05-28 22:22:42 -0300

    Update visualizar-personais.php

--- visualizar-personais.php
+++ visualizar-personais.php
@@ -12,7 +12,7 @@ $result = $conex->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Visualizar Personais Cadastrados - GymON</title>
+    <title>Visualizar Personais - GymON</title>
     <link rel="stylesheet" href="css/styleadm.css"> 
     <link rel="icon" href="assets/logo-gymon.jpeg" type="image/x-icon">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>

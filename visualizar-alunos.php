1732170426ffd4b18bb31904377129fc4273d24f
Pedro Freiberger Schneider <pedrofrschneider@gmail.com>
2024-06-03 21:59:05 -0300

    responsividade adm

--- visualizar-alunos.php
+++ visualizar-alunos.php
@@ -2,7 +2,6 @@
 
 include_once('cfg.php');
 
-
 $sql = "SELECT * FROM Aluno";
 $result = $conex->query($sql);
 ?>
@@ -13,12 +12,15 @@ $result = $conex->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visualizar Alunos - GymON</title>
-    <link rel="stylesheet" href="css/style_adm.css"> 
+    <link rel="stylesheet" href="css/styleadm.css"> 
     <link rel="icon" href="assets/logo-gymon.jpeg" type="image/x-icon">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <?php include 'menu-admin.php'; ?>
     <h1>Lista de Alunos</h1>
+    <input type="text" id="searchInput" placeholder="Pesquisar por nome..." onkeyup="filterTable()">
+    <div id="message"></div>
     <table>
         <thead>
             <tr>
@@ -32,17 +34,38 @@ $result = $conex->query($sql);
             <?php while ($row = $result->fetch_assoc()): ?>
             <tr>
                 <td><?php echo $row['ID_Aluno']; ?></td>
-                <td><?php echo $row['Nome_Aluno']; ?></td>
+                <td class="aluno-name"><?php echo $row['Nome_Aluno']; ?></td>
                 <td><?php echo $row['Email_Aluno']; ?></td>
                 <td><?php echo $row['DataNasc_Aluno']; ?></td>
             </tr>
             <?php endwhile; ?>
         </tbody>
     </table>
+
+    <script>
+        function filterTable() {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("searchInput");
+            filter = input.value.toUpperCase();
+            table = document.querySelector("table tbody");
+            tr = table.getElementsByTagName("tr");
+
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByClassName("aluno-name")[0];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
+    </script>
 </body>
 </html>
 
 <?php
-
 $conex->close();
 ?>
--- visualizar-personais.php
+++ visualizar-personais.php
@@ -2,7 +2,6 @@
 
 include_once('cfg.php');
 
-
 $sql = "SELECT * FROM Personal";
 $result = $conex->query($sql);
 ?>
@@ -13,13 +12,14 @@ $result = $conex->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visualizar Personais Cadastrados - GymON</title>
-    <link rel="stylesheet" href="css/style_adm.css"> 
+    <link rel="stylesheet" href="css/styleadm.css"> 
     <link rel="icon" href="assets/logo-gymon.jpeg" type="image/x-icon">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <?php include 'menu-admin.php'; ?>
     <h1>Lista de Personais</h1>
+    <input type="text" id="searchInput" placeholder="Pesquisar por nome..." onkeyup="filterTable()">
     <div id="message"></div>
     <table>
         <thead>
@@ -35,7 +35,7 @@ $result = $conex->query($sql);
             <?php while ($row = $result->fetch_assoc()): ?>
             <tr id="personal-<?php echo $row['ID_Personal']; ?>">
                 <td><?php echo $row['ID_Personal']; ?></td>
-                <td><?php echo $row['Nome_Personal']; ?></td>
+                <td class="personal-name"><?php echo $row['Nome_Personal']; ?></td>
                 <td><?php echo $row['Email_Personal']; ?></td>
                 <td class="status">
                     <?php echo $row['Status_Personal'] == 1 ? 'Aprovado' : ($row['Status_Personal'] == 0 ? 'Negado' : 'Em Avaliação'); ?>
@@ -67,6 +67,26 @@ $result = $conex->query($sql);
                 });
             });
         });
+
+        function filterTable() {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("searchInput");
+            filter = input.value.toUpperCase();
+            table = document.querySelector("table tbody");
+            tr = table.getElementsByTagName("tr");
+
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByClassName("personal-name")[0];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
     </script>
 </body>
 </html>
